678c04abdc480caeea700f58f8624c6ace474334
Javier S. Orejarena <32186552+JaviAir@users.noreply.github.com>
2022-06-02 19:31:58 -0400

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const apolloClient = new ApolloClient({
+  // uri: "https://graphql.org/swapi-graphql",
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+  // return <Component {...pageProps} />
+}
+export default MyApp;
